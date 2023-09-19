9b49396036b34d163c65c7cdb220ce1373c9377c
Shahariar Sohan <Sohanshahariar4@gmail.com>
2023-09-19 10:35:16 +0600

    gym card modification has completed

--- src/Components/Features/Features.jsx
+++ src/Components/Features/Features.jsx
@@ -1,9 +1,9 @@
 import PropTypes from "prop-types";
-
+import { BsListCheck } from "react-icons/bs";
 const Features = ({ feature }) => {
   return (
-    <div>
-      <h1 className="font-semibold">{feature}</h1>
+    <div className="font-semibold flex gap-3 items-center">
+      <BsListCheck></BsListCheck> <h1>{feature}</h1>
     </div>
   );
 };
--- src/Components/PriceOption/PriceOption.jsx
+++ src/Components/PriceOption/PriceOption.jsx
@@ -4,15 +4,16 @@ import Features from "../Features/Features";
 const PriceOption = ({ gym }) => {
   const { name, features, price } = gym;
   return (
-    <div className="bg-blue-300 font-semibold w-full p-5 shadow-xl rounded-md">
+    <div className="bg-blue-300 font-semibold w-full p-5 shadow-xl rounded-md flex flex-col">
       <h1 className="text-3xl">{name}</h1>
       <h1 className="text-3xl">{price}$/Month</h1>
-      <div className="flex flex-col flex-grow">
+      <div className=" flex-grow">
+        <h1 className="text-xl">Features</h1>
         {features.map((feature, idx) => (
           <Features key={idx} feature={feature}></Features>
         ))}
       </div>
-      <btn className="text-white bg-red-600 hover:bg-red-800 px-10 py-2">
+      <btn className="text-white text-center rounded-md bg-red-600 hover:bg-red-800 px-10 py-2 mt-2">
         Select
       </btn>
     </div>
